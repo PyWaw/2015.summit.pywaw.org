bf4a14018a4953dbd3984fc63292a66d8b7e8148
Piotr Betkier <pbetkier@gmail.com>
2015-04-04 21:51:07 +0200

    Add accommodation suggestions page

--- summit/summit/urls.py
+++ summit/summit/urls.py
@@ -6,6 +6,7 @@ from registration.views import AttendeeCreateView, AttendeeDetailView, Attendees
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
     url(r'^code-of-conduct/$', TemplateView.as_view(template_name='code_of_conduct.html'), name='code-of-conduct'),
+    url(r'^accommodation/$', TemplateView.as_view(template_name='accommodation.html'), name='accommodation'),
     url(r'^registration/$', AttendeeCreateView.as_view(), name='attendee_create'),
     url(r'^registration/(?P<hash>[\w-]+)/$', AttendeeDetailView.as_view(), name='attendee_detail'),
     url(r'^attendees/$', AttendeesListView.as_view(), name='attendee_list'),
