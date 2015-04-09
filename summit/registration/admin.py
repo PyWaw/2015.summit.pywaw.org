12aed3f987a2c5d7ecfb26d08fa2bd1811843607
Wojtek Erbetowski <wojtek@erbetowski.pl>
2015-04-09 18:43:54 +0200

    Added 'marked as payed' admin action

--- summit/registration/admin.py
+++ summit/registration/admin.py
@@ -15,4 +15,13 @@ class AttendeeModelAdmin(admin.ModelAdmin):
     )
     readonly_fields = ('registration_date',)
 
+    actions = ['marked_as_payed']
+
+    def marked_as_payed(self, request, queryset):
+        rows_updated = queryset.update(is_paid=True)
+
+        self.message_user(request, "({}) attendees were marked as payed.".format(rows_updated))
+
+    marked_as_payed.short_description = "Marked as payed"
+
 admin.site.register(models.Attendee, AttendeeModelAdmin)
