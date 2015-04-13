fca1c520940967ef71928b44864cbde4ba5b64b4
Wojtek Erbetowski <wojtek@erbetowski.pl>
2015-04-13 13:58:27 +0200

    New admin action - mark invoice sent

--- summit/registration/admin.py
+++ summit/registration/admin.py
@@ -15,7 +15,7 @@ class AttendeeModelAdmin(admin.ModelAdmin):
     )
     readonly_fields = ('registration_date',)
 
-    actions = ['marked_as_payed']
+    actions = ['marked_as_payed', 'marked_invoice_sent']
 
     def marked_as_payed(self, request, queryset):
         rows_updated = queryset.update(is_paid=True)
@@ -23,5 +23,12 @@ class AttendeeModelAdmin(admin.ModelAdmin):
         self.message_user(request, "({}) attendees were marked as payed.".format(rows_updated))
 
     marked_as_payed.short_description = "Marked as payed"
+
+    def marked_invoice_sent(self, request, queryset):
+        rows_updated = queryset.update(invoice_sent=True)
+
+        self.message_user(request, "({}) attendees were marked as invoice sent.".format(rows_updated))
+
+    marked_as_payed.short_description = "Mark invoice sent"
 
 admin.site.register(models.Attendee, AttendeeModelAdmin)
