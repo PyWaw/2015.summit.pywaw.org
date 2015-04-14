e18249b00d1911fa83b206d9d1078b7828458c0d
Wojtek Erbetowski <wojtek@erbetowski.pl>
2015-04-14 17:03:50 +0200

    Modifying admin page

--- summit/registration/admin.py
+++ summit/registration/admin.py
@@ -28,14 +28,14 @@ class AttendeesTypesListFilter(admin.SimpleListFilter):
             'speakers': dict(is_speaker=True),
 
             'paid': dict(is_organizer=False,
-                          is_volunteer=False,
-                          is_speaker=False,
-                          is_paid=True),
+                         is_volunteer=False,
+                         is_speaker=False,
+                         is_paid=True),
 
             'not_paid': dict(is_organizer=False,
-                              is_volunteer=False,
-                              is_speaker=False,
-                              is_paid=False),
+                             is_volunteer=False,
+                             is_speaker=False,
+                             is_paid=False),
 
             'need_invoice': dict(is_organizer=False,
                                  is_volunteer=False,
@@ -63,22 +63,22 @@ class AttendeeModelAdmin(admin.ModelAdmin):
 
     readonly_fields = ('registration_date',)
 
-    actions = ['marked_as_paid', 'marked_invoice_sent']
+    actions = ['mark_as_paid', 'mark_invoice_sent']
 
     list_filter = (AttendeesTypesListFilter,)
 
-    def marked_as_paid(self, request, queryset):
+    def mark_as_paid(self, request, queryset):
         rows_updated = queryset.update(is_paid=True)
 
         self.message_user(request, "({}) attendees were marked as paid.".format(rows_updated))
 
-    marked_as_paid.short_description = "Marked as paid"
+    mark_as_paid.short_description = "Marked as paid"
 
-    def marked_invoice_sent(self, request, queryset):
+    def mark_invoice_sent(self, request, queryset):
         rows_updated = queryset.update(invoice_sent=True)
 
         self.message_user(request, "({}) attendees were marked as invoice sent.".format(rows_updated))
 
-    marked_as_paid.short_description = "Mark invoice sent"
+    mark_as_paid.marked_invoice_sent = "Mark invoice sent"
 
 admin.site.register(models.Attendee, AttendeeModelAdmin)
