ef6936b2fe962d5d01c74ec3f6eb30f20fbbbc00
haxoza <prze.lewandowski@gmail.com>
2015-05-08 16:00:08 +0200

    Add speakers guide.

--- summit/summit/urls.py
+++ summit/summit/urls.py
@@ -10,5 +10,6 @@ urlpatterns = patterns('',
     url(r'^registration/$', AttendeeCreateView.as_view(), name='attendee_create'),
     url(r'^registration/(?P<hash>[\w-]+)/$', AttendeeDetailView.as_view(), name='attendee_detail'),
     url(r'^attendees/$', AttendeesListView.as_view(), name='attendee_list'),
+    url(r'^speakers-guide/$', TemplateView.as_view(template_name='speakers_guide.html'), name='speakers-guide'),
     url(r'^admin/', include(admin.site.urls)),
 )
