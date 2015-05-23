31c7a3b2aa18599a36bf3305500a0d13aaeae73e
haxoza <prze.lewandowski@gmail.com>
2015-05-23 23:16:11 +0200

    Registration closed.

--- summit/summit/urls.py
+++ summit/summit/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, url, include
 from django.contrib import admin
-from django.views.generic import TemplateView
-from registration.views import AttendeeCreateView, AttendeeDetailView, AttendeesListView
+from django.views.generic import TemplateView, RedirectView
+from registration.views import AttendeeDetailView, AttendeesListView
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
     url(r'^code-of-conduct/$', TemplateView.as_view(template_name='code_of_conduct.html'), name='code-of-conduct'),
     url(r'^accommodation/$', TemplateView.as_view(template_name='accommodation.html'), name='accommodation'),
-    url(r'^registration/$', AttendeeCreateView.as_view(), name='attendee_create'),
+    url(r'^registration/$', RedirectView.as_view(pattern_name='home'), name='attendee_create'),
     url(r'^registration/(?P<hash>[\w-]+)/$', AttendeeDetailView.as_view(), name='attendee_detail'),
     url(r'^attendees/$', AttendeesListView.as_view(), name='attendee_list'),
     url(r'^speakers-guide/$', TemplateView.as_view(template_name='speakers_guide.html'), name='speakers-guide'),
