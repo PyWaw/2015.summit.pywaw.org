7fcfe908b59623ee87e545933f6647216a1baa60
konradhalas <halas.konrad@gmail.com>
2018-01-04 00:37:14 +0100

    Change uwsgi setup - part II

--- containers/run.sh
+++ containers/run.sh
@@ -1,4 +1,4 @@
 ../containers/wait-for-it.sh -t 60 db:5432
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-uwsgi --socket :8000 --mount /2015=summit/wsgi.py --processes 4 --threads 2 --manage-script-name
+uwsgi --socket :8000 --mount /2015=summit/wsgi.py --processes 4 --threads 2 --manage-script-name --callable application
